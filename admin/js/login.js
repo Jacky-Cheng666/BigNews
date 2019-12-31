0cb6cf1159b9ba806538b8fca7ab3ed883086b54
jacky <1260027439@qq.com>
2019-12-31 23:25:02 +0800

    完成登录模块

---
+++ admin/js/login.js
@@ -0,0 +1,56 @@
+// 入口函数
+/*
+    1，登录按钮点击事件
+      1.1获取输入框文本
+      1.2非空判断
+      1.3发送ajax请求
+      1.4跳转首页
+*/
+$(function () {
+    $(".input_sub").click(function (e) {
+        // 阻止表单默认跳转行为
+        e.preventDefault();
+        // 1，获取表单文本
+        var $username = $(".input_txt").val().trim();
+        var $pwd = $(".input_pass").val().trim();
+        // 2，非空判断
+        if ($username.length == 0 || $pwd.length == 0) {
+            // alert('用户名和密码不能为空')
+            $('#login_msg').modal();
+            $(".modal-body p").text("用户名和密码不能为空");
+            return;
+        }
+        // 3，发送ajax请求
+        $.ajax({
+            url: 'http://localhost:8080/api/v1/admin/user/login',
+            type: 'post',
+            dataType: 'json',
+            data: {
+                username: $username,
+                password: $pwd
+            },
+            success: function (backData) {
+                console.log(backData);
+                if (backData.code == 200) {
+                    $('#login_msg').modal();
+                    $(".modal-body>p").text("登录成功");
+                    $('#login_msg').on('hidden.bs.modal', function () {
+                        window.location.href = './index.html';
+                    });
+
+                } else {
+                    $('#login_msg').modal();
+                    $(".modal-body>p").text(backData.msg);
+                    $('#login_msg').on('hidden.bs.modal', function () {
+                        $(".input_txt").val("");
+                        $(".input_pass").val("");
+                    });
+
+                }
+            }
+        });
+
+
+
+    });
+});
